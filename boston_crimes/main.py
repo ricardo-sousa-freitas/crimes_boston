cc15227978febe18486cca2cf3e6f1d28c60c7fd
Ricardo Sousa Freitas <ricardo.sousa@altran.es>
2021-07-12 19:37:01 +0200

    initial changes

---
+++ boston_crimes/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def print_test():
+    print('TEST')
+    df = pd.DataFrame(['a', 'b'],['c','d'])
+    print(df)
+
+
+
+if __name__ == '__main__':
+    print_test()
--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+from setuptools import setup, find_packages
+#from requirements import required
 
+PACKAGE_NAME = "boston_crimes"
 
-print('hello')
+setup(name=PACKAGE_NAME,
+    packages=find_packages(),
+    entry_points={'console_scripts': [f'{PACKAGE_NAME} = {PACKAGE_NAME}.main:print_test']}),
+    # install_requires = ["numpy==1.14.5",
+    #             "pandas==0.24.2"])
